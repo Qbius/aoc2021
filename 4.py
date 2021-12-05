f45dcf6924dd3e5827629555e38ac398d89866b1
Michal Surowka <theqbius@gmail.com>
2021-12-05 15:05:37 +0000

    some common code in days 4 and 5

--- 4.py
+++ 4.py
@@ -14,22 +14,21 @@ def find_winning(drawns, boards):
     winning = [(board, selected) for board, selected in boards if is_winning(selected)]
     return (drawn, winning[0]) if len(winning) > 0 else find_winning(rest_drawns, boards)
 
-def first(base: raw_input):
-    all_drawns, *all_boards = base.split('\n\n')
-    drawns = list(map(int, all_drawns.split(',')))
-    boards = [(list(chain(*[list(map(int, filter(lambda x: x, row.split(' ')))) for row in board.split('\n')])), []) for board in all_boards]
-    last, (board, selected) = find_winning(drawns, boards)
-    return last * sum(e for i, e in enumerate(board) if i not in selected)
-
-def find_winning_continuous(drawns, boards):
+def find_last_winning(drawns, boards):
     drawn, *rest_drawns = drawns
     boards = [(board, [*selected, next(i for i, e in enumerate(board) if e == drawn)] if drawn in board else selected) for board, selected in boards]
     losing = [(board, selected) for board, selected in boards if not is_winning(selected)]
-    return (drawn, boards[0]) if len(losing) == 0 else find_winning_continuous(rest_drawns, losing)  
+    return (drawn, boards[0]) if len(losing) == 0 else find_last_winning(rest_drawns, losing)
 
-def second(base: raw_input):
+def get_board_score(base, find_f):
     all_drawns, *all_boards = base.split('\n\n')
     drawns = list(map(int, all_drawns.split(',')))
     boards = [(list(chain(*[list(map(int, filter(lambda x: x, row.split(' ')))) for row in board.split('\n')])), []) for board in all_boards]
-    last, (board, selected) = find_winning_continuous(drawns, boards)
+    last, (board, selected) = find_f(drawns, boards)
     return last * sum(e for i, e in enumerate(board) if i not in selected)
+
+def first(base: raw_input):
+    return get_board_score(base, find_winning)
+
+def second(base: raw_input):
+    return get_board_score(base, find_last_winning)
--- 5.py
+++ 5.py
@@ -4,9 +4,8 @@ from itertools import chain
 def sign(n):
     return (n // abs(n)) if n != 0 else 0
 
-def first(values: regex(r'(\d+),(\d+) -> (\d+),(\d+)', (int, int, int, int))):
-    values = [(x1, y1, x2, y2) for x1, y1, x2, y2 in values if x1 == x2 or y1 == y2]
-    to_points = [[(x1 + i * sign(x2 - x1), y1 + i * sign(y2 - y1)) for i in range((max(abs(x2 - x1), abs(y2 - y1)) + 1))] for x1, y1, x2, y2 in values]
+def find_doubled_points(lines):
+    to_points = [[(x1 + i * sign(x2 - x1), y1 + i * sign(y2 - y1)) for i in range((max(abs(x2 - x1), abs(y2 - y1)) + 1))] for x1, y1, x2, y2 in lines]
     
     singular = set()
     doubledd = set()
@@ -19,16 +18,9 @@ def first(values: regex(r'(\d+),(\d+) -> (\d+),(\d+)', (int, int, int, int))):
             singular.add(point)
     return len(doubledd)
 
+
+def first(values: regex(r'(\d+),(\d+) -> (\d+),(\d+)', (int, int, int, int))):
+    return find_doubled_points([(x1, y1, x2, y2) for x1, y1, x2, y2 in values if x1 == x2 or y1 == y2])
+
 def second(values: regex(r'(\d+),(\d+) -> (\d+),(\d+)', (int, int, int, int))):
-    to_points = [[(x1 + i * sign(x2 - x1), y1 + i * sign(y2 - y1)) for i in range((max(abs(x2 - x1), abs(y2 - y1)) + 1))] for x1, y1, x2, y2 in values]
-    
-    singular = set()
-    doubledd = set()
-    for point in chain(*to_points):
-        if point in doubledd:
-            pass
-        elif point in singular:
-            doubledd.add(point)
-        else:
-            singular.add(point)
-    return len(doubledd)
+    return find_doubled_points(values)

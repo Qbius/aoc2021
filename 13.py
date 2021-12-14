1d431ebd2e8b0a6c549c6f857a9901490a4729df
qbius <my@mail.com>
2021-12-14 14:03:17 +0100

    day 13

---
+++ 13.py
@@ -0,0 +1,48 @@
+def parse(raw_input):
+    dots_strs, fold_instructions_str = [part.split('\n') for part in raw_input.split('\n\n')]
+    dots = [list(map(int, dot_str.split(','))) for dot_str in dots_strs]
+    fold_instructions = [tuple(map(lambda x: int(x) if x.isdigit() else (x == 'x'), fold_str.split(' ')[-1].split('='))) for fold_str in fold_instructions_str]
+    return dots, fold_instructions
+
+def fold(dots, fold_instructions):
+    for is_horizontal, line in fold_instructions:
+        for i, dot in enumerate(dots):
+            relevant_index = 0 if is_horizontal else 1
+            if dot[relevant_index] > line:
+                dots[i][relevant_index] = line - (dot[relevant_index] - line)
+    return set(map(tuple, dots))
+
+def first(info):
+    dots, fold_instructions = info
+    fold_instructions = fold_instructions[:1]
+    return len(fold(dots, fold_instructions))
+
+def second(info):
+    dots, fold_instructions = info
+    folded_dots = fold(dots, fold_instructions)
+    max_x, max_y = max(x for x, y in folded_dots) + 1, max(y for x, y in folded_dots) + 1
+    return '\n' + '\n'.join(''.join('#' if (x, y) in folded_dots else ' ' for x in range(max_x)) for y in range(max_y))
+
+example = '''
+6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5
+'''

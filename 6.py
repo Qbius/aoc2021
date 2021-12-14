49dd952847049a99da69f6c0db03b04d9907cb22
qbius <my@mail.com>
2021-12-14 22:44:06 +0100

    6 to cache

--- 6.py
+++ 6.py
@@ -1,14 +1,12 @@
-example = '3,4,3,1,2'
+from functools import cache
 
 def parse(inpt):
     return list(map(int, inpt.split(',')))
 
-precalced = {}
+@cache
 def total_children(days):
-    if days not in precalced:
-        spawned = list(range(1, days, 7))
-        precalced[days] = len(spawned) + sum(total_children(days - spawn - 8) for spawn in spawned)
-    return precalced[days]
+    spawned = list(range(1, days, 7))
+    return len(spawned) + sum(total_children(days - spawn - 8) for spawn in spawned)
 
 def total_by_timers(timers, days):
     return len(timers) + sum(total_children(days - timer + 1) for timer in timers)
@@ -18,4 +16,5 @@ def first(timers):
 
 def second(timers):
     return total_by_timers(timers, 256)
-
+    
+example = '3,4,3,1,2'
